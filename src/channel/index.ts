1dfad3bcb7be2996618b6b4c5c52355873093251
jane-alea <107285378+jane-alea@users.noreply.github.com>
2026-04-11 02:40:53 -0400

    Add missing methods to channel interfaces

--- src/channel/index.ts
+++ src/channel/index.ts
@@ -10,6 +10,8 @@ export interface ClientSideTransferChannel {
     event: K,
     callback: (...args: ClientTransferEventMap[K]) => void,
   ): void;
+
+  sendChange(number: number, change: Change): void;
 }
 
 export interface ServerTransferEventMap {
@@ -17,6 +19,23 @@ export interface ServerTransferEventMap {
   connection: [client: Client];
   disconnection: [client: Client];
 }
+
+export interface ServerSideTransferChannel {
+  on<K extends keyof ServerTransferEventMap>(
+    event: K,
+    callback: (...args: ServerTransferEventMap[K]) => void,
+  ): void;
+
+  acceptChange(client: Client, number: number): void;
+  sendMergeConflict(
+    client: Client,
+    lastApproved: number,
+    changeset: Change[],
+  ): void;
+  sendThirdPartyChange(client: Client, number: number, change: Change): void;
+  kick(client: Client): void;
+}
+
 export interface Change {}
 
 export interface Client {
