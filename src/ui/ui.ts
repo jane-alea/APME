7575d1348b63832327e11b51ecd368a4888b6973
jane-alea <107285378+jane-alea@users.noreply.github.com>
2026-04-13 17:23:39 -0400

    UI: `TextIconButton`

--- src/ui/ui.ts
+++ src/ui/ui.ts
@@ -36,3 +36,57 @@ export class APMEUI {
     return ui;
   }
 }
+
+export abstract class BaseUIComponent {
+  // REQUIREMENT: innerContainer must be a descendant of outerContainer, or equal to it
+  abstract innerContainer: HTMLElement;
+  abstract outerContainer: HTMLElement;
+  abstract ui: APMEUI;
+  abstract children: BaseUIComponent[];
+
+  textButton(icon: string, text: string, action: VoidFunction) {
+    const button = new TextIconButton(this.ui, icon, text, action);
+    this.children.push(button);
+    this.innerContainer.appendChild(button.outerContainer);
+  }
+
+  destroy() {
+    for (const child of this.children) {
+      child.destroy();
+    }
+
+    this.outerContainer.remove();
+  }
+}
+
+export class TextIconButton extends BaseUIComponent {
+  innerContainer: HTMLElement;
+  outerContainer: HTMLElement;
+  iconElement: HTMLImageElement;
+  textElement: HTMLElement;
+  children: BaseUIComponent[] = [];
+  ui: APMEUI;
+
+  constructor(ui: APMEUI, icon: string, text: string, action: VoidFunction) {
+    super();
+    this.ui = ui;
+
+    this.innerContainer = document.createElement("div");
+    this.innerContainer.classList.add("icon-button");
+    this.outerContainer = this.innerContainer;
+
+    this.iconElement = document.createElement("img");
+    this.iconElement.classList.add("icon");
+    this.iconElement.src = ui.icons.get(icon) || "";
+    this.innerContainer.appendChild(this.iconElement);
+
+    this.textElement = document.createElement("div");
+    this.textElement.classList.add("label");
+    this.textElement.innerText = text;
+    this.innerContainer.appendChild(this.textElement);
+
+    this.innerContainer.addEventListener("click", (event) => {
+      if (event.button === 0) action();
+    });
+  }
+}
